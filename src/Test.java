7a1de8f45f9207940c653959cf2f2e7d6280c788
stari <starinenko2251@gmail.com>
2023-04-14 10:45:31 +0600

    019 Ключевое слово synchronized I часть

--- src/Test.java
+++ src/Test.java
@@ -1,28 +1,38 @@
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-        MyThread myThreed = new MyThread();
-        myThreed.start();
-
-        Scanner scanner =new Scanner(System.in);
-        scanner.nextLine();
-        myThreed.shutdown();
+    private int counter=0;
+    public static void main(String[] args) throws InterruptedException {
+        Test test=new Test();
+        test.doWork();
     }
-}
-class MyThread extends Thread{
-    private volatile boolean running=true;
-    public void run() {
-        while (running) {
-            System.out.println("Hello");
-            try {
-                Thread.sleep(100);
-            }catch (InterruptedException e) {
-                e.printStackTrace();
+    public synchronized void increment(){
+        counter++;
+    }
+    public  void doWork() throws InterruptedException {
+        Thread thread1=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0; i<10000; i++){
+                    increment();
+                }
             }
-        }
+        });
+        Thread thread2=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0; i<10000; i++){
+
+                    increment();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(counter);
     }
-    public  void shutdown() {
-        this.running=false;
-    }
+
 }
