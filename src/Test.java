6a30b3458aaf781694fdecca2dbb4f614de2b6e2
stari <starinenko2251@gmail.com>
2023-04-15 16:19:14 +0600

    020 Ключевое слово synchronized II часть

--- src/Test.java
+++ src/Test.java
@@ -1,38 +1,83 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Test {
-    private int counter=0;
+    private int counter = 0;
+
     public static void main(String[] args) throws InterruptedException {
-        Test test=new Test();
-        test.doWork();
+        Worker test = new Worker();
+        test.main();
     }
-    public synchronized void increment(){
-        counter++;
+
+
+}
+
+class Worker {
+    Random random = new Random();
+    Object lock1 = new Object();
+    Object lock2 = new Object();
+    private List<Integer> list1 = new ArrayList<>();
+    private List<Integer> list2 = new ArrayList<>();
+
+    public void addToList1() {
+        synchronized (lock1) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            list1.add(random.nextInt(100));
+        }
+
     }
-    public  void doWork() throws InterruptedException {
-        Thread thread1=new Thread(new Runnable() {
+
+    public void addToList2() {
+        synchronized (lock2) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            list2.add(random.nextInt(100));
+        }
+    }
+
+    public void work() {
+        for (int i = 0; i < 1000; i++) {
+            addToList1();
+            addToList2();
+        }
+    }
+
+    public void main() {
+        long begin = System.currentTimeMillis();
+        Thread thread1 = new Thread(new Runnable() {
+
             @Override
             public void run() {
-                for(int i=0; i<10000; i++){
-                    increment();
-                }
+                work();
             }
         });
-        Thread thread2=new Thread(new Runnable() {
+        Thread thread2 = new Thread(new Runnable() {
+
             @Override
             public void run() {
-                for(int i=0; i<10000; i++){
-
-                    increment();
-                }
+                work();
             }
         });
-
         thread1.start();
         thread2.start();
-        thread1.join();
-        thread2.join();
-        System.out.println(counter);
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("time: " + (end - begin));
+        System.out.println("list1:" + list1.size());
+        System.out.println("list2:" + list2.size());
     }
-
 }
