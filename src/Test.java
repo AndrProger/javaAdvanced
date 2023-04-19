f7ff9a8d186193ec25676f6d4967a19685a90219
stari <starinenko2251@gmail.com>
2023-04-19 13:47:05 +0600

    032 Классы Pattern и Matcher

--- src/Test.java
+++ src/Test.java
@@ -3,18 +3,19 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test {
 
     public static void main(String[] args) {
-        String str ="hello there hey";
-        String[] strArr=str.split(" ");
-        System.out.println(Arrays.toString(strArr));
+        String test=":rfwewfwe tgwflwrle, star@gmail.com " +
+                "reewf rwe e ertewfw tewfwe re@yandex.ru";
+        Pattern email=Pattern.compile("(\\w+)@(gmail|yandex)\\.(com|ru)");
+        Matcher matcher= email.matcher(test);
 
-        String b ="hello122342312there31232hey";
-        b=b.replaceFirst("\\d+",".");
-        // b=b.replaceAll("\\d+",".");
-        System.out.println(b);
-
+        while (matcher.find()){
+            System.out.println(matcher.group(1));
+        }
     }
 }
