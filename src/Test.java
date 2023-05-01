f852d2d894fa6790a6f08f5e8c4a21eb3bc472bf
stari <starinenko2251@gmail.com>
2023-05-01 19:03:47 +0600

    033 Лямбда - выражения часть I

--- src/Test.java
+++ src/Test.java
@@ -6,16 +6,23 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+
 public class Test {
-
     public static void main(String[] args) {
-        String test=":rfwewfwe tgwflwrle, star@gmail.com " +
-                "reewf rwe e ertewfw tewfwe re@yandex.ru";
-        Pattern email=Pattern.compile("(\\w+)@(gmail|yandex)\\.(com|ru)");
-        Matcher matcher= email.matcher(test);
+        List<String> list =new ArrayList<>();
+        list.add("hello");
+        list.add("goodbye");
+        list.add("a");
+        list.add("ab");
 
-        while (matcher.find()){
-            System.out.println(matcher.group(1));
-        }
+        Comparator<String> comparator = (String a, String b) ->{
+            if(a.length()>b.length()) return 1;
+            if(a.length()==b.length()) return 0;
+            else  return  -1;
+        };
+        list.sort(comparator);
+        System.out.println(list);
     }
-}
+
+   }
