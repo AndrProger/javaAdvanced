d0d1b79723973ce7f470767044a0fd970f2912e9
stari <starinenko2251@gmail.com>
2023-05-04 18:54:01 +0600

    041 Аннотации. Создание своих аннотаций

--- src/Test.java
+++ src/Test.java
@@ -1,37 +1,13 @@
-import java.lang.annotation.Target;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+
 
 
 public class Test {
     public static void main(String[] args) {
-       int[]arr=new int[10];
-       List<Integer> list =new ArrayList<Integer>();
 
-       fillArr(arr);
-       fillList(list);
 
-        System.out.println(list);
-        System.out.println(Arrays.toString(arr));
-
-        arr=Arrays.stream(arr).map(a->a*2).toArray();
-        list.stream().map(a->a*2).collect(Collectors.toList());
     }
-
-    private static void fillArr(int[] arr) {
-        for (int i=0; i<arr.length; i++){
-            arr[i] = i+1;
-        }
+    @MethodInfo(author = "Andrew",dateOfCreation = 2023,purpose = "Print Hello World")
+    public void printHelloWorld(){
+        System.out.println("Hello World");
     }
-    private static void fillList(List list) {
-        for (int i=0; i<10;i++){
-            list.add(i+1);
-        }
-    }
-
 }
