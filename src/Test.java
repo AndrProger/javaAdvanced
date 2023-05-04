56379041d5e56c4b518acf0e875e56427ab927b0
stari <starinenko2251@gmail.com>
2023-05-04 16:32:04 +0600

    034 Лямбда - выражения часть II

--- src/Test.java
+++ src/Test.java
@@ -5,24 +5,33 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import java.util.stream.Collectors;
 
 
 public class Test {
     public static void main(String[] args) {
-        List<String> list =new ArrayList<>();
-        list.add("hello");
-        list.add("goodbye");
-        list.add("a");
-        list.add("ab");
+       int[]arr=new int[10];
+       List<Integer> list =new ArrayList<Integer>();
 
-        Comparator<String> comparator = (String a, String b) ->{
-            if(a.length()>b.length()) return 1;
-            if(a.length()==b.length()) return 0;
-            else  return  -1;
-        };
-        list.sort(comparator);
+       fillArr(arr);
+       fillList(list);
+
         System.out.println(list);
+        System.out.println(Arrays.toString(arr));
+
+        arr=Arrays.stream(arr).map(a->a*2).toArray();
+        list.stream().map(a->a*2).collect(Collectors.toList());
     }
 
-   }
+    private static void fillArr(int[] arr) {
+        for (int i=0; i<arr.length; i++){
+            arr[i] = i+1;
+        }
+    }
+    private static void fillList(List list) {
+        for (int i=0; i<10;i++){
+            list.add(i+1);
+        }
+    }
+
+}
